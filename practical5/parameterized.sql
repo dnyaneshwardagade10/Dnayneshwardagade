d09d885630609684b4905fbef0eebe94dc98ed43
dnyaneshwardagade10 <dnyaneshwardagade07@gmail.com>
2025-09-17 11:43:32 +0530

    parameterized.sql

--- practical5/parameterized.sql
+++ practical5/parameterized.sql
@@ -1,28 +1,35 @@
-declare
-  cursor product_cursor(p_price_1 number) is 
-    select pid, pname, price 
-    from product 
-    where price > p_price_1;
+DECLARE
+  CURSOR product_cursor(p_price_1 NUMBER) IS 
+    SELECT pid, pname, price 
+    FROM product 
+    WHERE price > p_price_1;
 
-  p_pid   product.pid%type;
-  p_pname product.pname%type;
-  p_price product.price%type;
-begin
-  dbms_output.put_line('starting to fetch products above price p_price_1...');
+  p_pid   product.pid%TYPE;
+  p_pname product.pname%TYPE;
+  p_price product.price%TYPE;
 
-  open product_cursor(50);
-  loop
-    fetch product_cursor into p_pid, p_pname, p_price;
-    exit when product_cursor%notfound;
-    dbms_output.put_line('pid: ' || p_pid || ', name: ' || p_pname || ', price: ' || p_price);
-  end loop;
-  close product_cursor;
+  v_input NUMBER;  -- variable to hold user input
+BEGIN
+  -- Take input from user
+  v_input := &enter_price;
 
-  dbms_output.put_line('done fetching products.');
-exception
-  when others then
-    dbms_output.put_line('an error occurred: ' || sqlerrm);
-end;
+  DBMS_OUTPUT.PUT_LINE('Starting to fetch products above price ' || v_input || '...');
+
+  OPEN product_cursor(v_input);
+  LOOP
+    FETCH product_cursor INTO p_pid, p_pname, p_price;
+    EXIT WHEN product_cursor%NOTFOUND;
+    DBMS_OUTPUT.PUT_LINE('pid: ' || p_pid || ', name: ' || p_pname || ', price: ' || p_price);
+  END LOOP;
+  CLOSE product_cursor;
+
+  DBMS_OUTPUT.PUT_LINE('Done fetching products.');
+EXCEPTION
+  WHEN OTHERS THEN
+    DBMS_OUTPUT.PUT_LINE('An error occurred: ' || SQLERRM);
+END;
 /
+
+
 
 
